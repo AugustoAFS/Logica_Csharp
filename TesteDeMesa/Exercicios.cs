2b369855201ed7e0f4a97d1dbc0c98021495a370
AugustoAFS <augusto.farias.santo03@gmail.com>
2024-11-11 12:30:30 -0300

    Teste de Mesa ATV 1,2,3

---
+++ TesteDeMesa/Exercicios.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDeMesa
+{
+    internal class Exercicios
+    {
+        public static int[] Exercicio1()
+        {
+            int a = 10; 
+            int b = 20;
+            int c = ((a + b) / 2);
+            c -= 40;
+            int resultado = (a + b) + c;
+            int[] vetor = new int[5];
+            for (int i = 0; i <= 4; i++)
+            {
+               
+                if (i == 3)
+                {
+                    vetor[i] = resultado;
+                }
+                else
+                {
+                    vetor[i] = 0;
+                }
+                
+            }
+
+            return vetor;
+        }
+        public static int[] Exercicio2() 
+        {
+            int a = 2;
+            int[] vetor = new int[6];
+            while (a < 6)
+            {
+                vetor[a] = 10 * a; 
+                a++;
+            }
+            return vetor;
+        }
+
+        public static int[] Exercicio3() 
+        {
+            int a = 7;
+            int b = a - 6;
+            int[] vetor = new int[8];
+            while (b < a)
+            {
+                vetor[b] = b + a;
+                b += 2;
+            }
+            return vetor;
+        }
+    }
+}
---
+++ TesteDeMesa/Program.cs
@@ -0,0 +1,54 @@
+﻿using TesteDeMesa;
+
+Console.WriteLine("Exercicios Para Teste de Mesa");
+
+char continuar;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Escolha um Exercicio para conferir o resultado:");
+    Console.WriteLine("1:Exercicio 1\n2:Exercicio 2\n3:Exercicio 3");
+    int opcao = Convert.ToInt32(Console.ReadLine());
+
+    switch(opcao)
+    {
+        case 1:
+            {
+               int[] vetor =  Exercicios.Exercicio1();
+                for (int i = 0;i < 4;i++)
+                {
+                    if (i == 3)Console.WriteLine(vetor[i]);
+                    
+                }
+                break;
+            }
+        case 2:
+            {
+                int[] vetor = Exercicios.Exercicio2();
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.WriteLine(vetor[i]);
+
+                }
+                break;
+            }
+        case 3:
+            {
+                int[] vetor = Exercicios.Exercicio3();
+                for (int i = 0; i < 7; i++)
+                {
+                    Console.WriteLine(vetor[i]);
+
+                }
+                break;
+            }
+        default:
+            {
+                break;
+            }
+    }
+
+    Console.WriteLine("Deseja continuar?(S/N)");
+    continuar = Convert.ToChar(Console.ReadLine());
+}while (continuar == 'S' || continuar == 's');
